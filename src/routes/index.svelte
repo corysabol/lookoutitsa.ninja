cd2770066c7e34162e906339c872a20f0ec1351b
Cory Sabol <cory@secureideas.com>
2019-06-08 23:15:38 -0400

    setting up circleci build and deploy to gh-pages

---
+++ src/routes/index.svelte
@@ -0,0 +1,48 @@
+<style>
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Don't move a muscle</title>
+</svelte:head>
+
+<h1>I am but a shadow.</h1>
+
+<!-- TODO: Replace this with a sick ninja png / svg
+<figure>
+	<img alt='Borat' src='great-success.png'>
+	<figcaption>HIGH FIVE!</figcaption>
+</figure>
+-->
+
+<p><strong>This site is under construction. Come back later for some ninja shit.</strong></p>
